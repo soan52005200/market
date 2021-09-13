83067cb789bbc8b38cc78a056b7abe763163de0f
Андрей <soan52005200@gmail.com>
2021-09-14 01:04:19 +0300

    14.09

---
+++ src/main/java/ru/sfedu/market/Constants.java
@@ -0,0 +1,10 @@
+package ru.sfedu.market;
+
+public final class Constants {
+
+    public static final String ENV_PROPERTIES = "env";
+    public static final String LOG4J2_PROPERTIES = "log4j2.configurationFile";
+    public static final String ENV_DEFAULT = "./src/main/resources/enviroment.properties";
+
+
+}
---
+++ src/main/java/ru/sfedu/market/Main.java
@@ -0,0 +1,16 @@
+package ru.sfedu.market;
+
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
+public class Main {
+
+    private static final Logger logger = LogManager.getLogger(Main.class.getName());
+
+    public static void main(String... args){
+        logger.error("Hello world(error)");
+        logger.debug("Hello world(debug)");
+        logger.info("Hello world(info)");
+    }
+
+}
