f78fc47a93417b03df3ee245841bb302dcbb759f
Андрей <soan52005200@gmail.com>
2021-09-28 17:01:57 +0300

    28.09

---
+++ src/test/java/ru/sfedu/market/api/TestPrintConstantTest.java
@@ -0,0 +1,22 @@
+package ru.sfedu.market.api;
+
+import ru.sfedu.market.Constants.*;
+import org.junit.jupiter.api.Test;
+import ru.sfedu.market.utils.ConfigurationUtil;
+
+import java.io.IOException;
+
+import static ru.sfedu.market.Constants.FIRST_TEST_RESULT;
+
+class TestPrintConstantTest {
+    @Test
+    void name() {
+        try {
+            System.out.println(ConfigurationUtil.getConfigurationEntry(FIRST_TEST_RESULT));
+        }
+        catch (IOException e)
+        {
+            System.out.println(e);
+        }
+    }
+}
