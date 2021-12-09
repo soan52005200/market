20365a5d60edf6a3703ee50d7218766f8b9564fe
user <soan52005200@gmail.com>
2021-12-09 23:54:12 +0300

    UPDATE EATABLE AND UNEATABLE TESTS CORRECTED

--- src/main/java/ru/sfedu/market/bean/ProductTypeConverter.java
+++ src/main/java/ru/sfedu/market/bean/ProductTypeConverter.java
@@ -3,13 +3,8 @@ package ru.sfedu.market.bean;
 import com.opencsv.bean.AbstractBeanField;
 import com.opencsv.exceptions.CsvConstraintViolationException;
 import com.opencsv.exceptions.CsvDataTypeMismatchException;
-import ru.sfedu.market.api.DataProviderCSV;
-import ru.sfedu.market.api.IDataProvider;
-
 
 public class ProductTypeConverter extends AbstractBeanField<Product> {
-
-    private final IDataProvider csv = new DataProviderCSV();
 
     @Override
     protected Object convert(String s) throws CsvDataTypeMismatchException, CsvConstraintViolationException {
